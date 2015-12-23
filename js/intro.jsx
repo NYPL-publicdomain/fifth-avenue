bfe359c47ae4ee942bb019666c5a139da7d347d3
Bert Spaan <bert.spaan@gmail.com>
2015-12-23 18:43:40 -0500

    less indent

--- js/intro.jsx
+++ js/intro.jsx
@@ -12,15 +12,14 @@ module.exports = React.createClass({
           From <a href='http://digitalcollections.nypl.org/collections/fifth-avenue-new-york-from-start-to-finish#/?tab=about'>The New York Public Library Digital Collections</a>:
         </p>
         <blockquote className='columns'>
+          Fifth Avenue, the street that became the social and cultural spine of New York's elite, first appeared on the Commissioners&#39; Map
+          of 1811. At that time, it was merely a country road to Yorkville (then just a tiny self-contained village), but in the proposed
+          grid plan it would be a grand boulevard. As the City grew and prospered Fifth Avenue became synonymous with fashionable life,
+          the site of mansions, cultural and social institutions, and restaurants and shops catering to the elite.
 
-            Fifth Avenue, the street that became the social and cultural spine of New York's elite, first appeared on the Commissioners&#39; Map
-            of 1811. At that time, it was merely a country road to Yorkville (then just a tiny self-contained village), but in the proposed
-            grid plan it would be a grand boulevard. As the City grew and prospered Fifth Avenue became synonymous with fashionable life,
-            the site of mansions, cultural and social institutions, and restaurants and shops catering to the elite.
-
-            In 1907, alarmed at the approach of factories, the leading merchants and residents formed the Fifth Avenue Association.
-            The <em>Save New York Committee</em> became a bulwark against the wrong kind of development. Perhaps inspired by this contemporary
-            movement, photographer Burton Welles used a wide-angled view camera in 1911 to document this most important street from Washington Square, north to East 93rd Street.
+          In 1907, alarmed at the approach of factories, the leading merchants and residents formed the Fifth Avenue Association.
+          The <em>Save New York Committee</em> became a bulwark against the wrong kind of development. Perhaps inspired by this contemporary
+          movement, photographer Burton Welles used a wide-angled view camera in 1911 to document this most important street from Washington Square, north to East 93rd Street.
         </blockquote>
         <p>
           With this <a href="//publicdomain.nypl.org">public domain remix</a>, you can compare the photos from the 1911 <a href="http://digitalcollections.nypl.org/collections/fifth-avenue-new-york-from-start-to-finish#/?tab=about">Fifth Avenue from Start to Finish collection</a> with
